b11133a6577c996456af72598cab06fdbc9ba779
Manojkumar <rkmanoj.srh@gmail.com>
2025-06-09 01:01:12 +0200

    Finished ISP_02 Task.

--- SOLID/ISP_02.java
+++ SOLID/ISP_02.java
@@ -3,16 +3,22 @@ package SOLID;
 public class ISP_02 {
     
     /*
-     * TASK:
-     * Again, some classes are more simple than other.
-     * Fix this without violating the ISP (Interface Segregation Principle)!
+     * FIX:
+     * Split the Worker interface into two interfaces: Workable and Restable.
+     * Developer implements both (works and rests).
+     * Robot implements only Workable (works, but does not rest).
+     * This respects ISP by not forcing classes to implement unused methods.
      */
-    public static interface Worker {
+
+    public static interface Workable {
         void work();
-        void rest();        
     }
 
-    public static class Developer implements Worker {
+    public static interface Restable {
+        void rest();
+    }
+
+    public static class Developer implements Workable, Restable {
         @Override
         public void work() {
             System.out.println("Developer is coding ...");
@@ -24,25 +30,32 @@ public class ISP_02 {
         }
     }
 
-    public static class Robot implements Worker {
+    public static class Robot implements Workable {
         @Override
         public void work() {
             System.out.println("Robot is working hard ...");
         }
-
-        @Override
-        public void rest() {
-            throw new UnsupportedOperationException("Robot does not need a break!");
-        }        
+        // No rest() method needed here
     }
 
-    public static void workingCycle(Worker worker) {
+    // Overloaded workingCycle methods based on capabilities
+    public static void workingCycle(Workable worker) {
         worker.work();
-        worker.rest();
+    }
+
+    public static void workingCycle(Workable worker, Restable restWorker) {
+        worker.work();
+        restWorker.rest();
     }
 
     public static void main(String[] args) {
-        workingCycle( new Developer() );
-        workingCycle( new Robot() );        
+        Developer developer = new Developer();
+        Robot robot = new Robot();
+
+        // Developer can work and rest
+        workingCycle(developer, developer);
+
+        // Robot only works
+        workingCycle(robot);
     }
 }
